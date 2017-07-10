91f7b85c9df3608f4ea559f7eddc5f0e4f976da6
Brian <Brian@LAPTOP-8VND5NEA>
2017-07-10 12:51:42 -0400

    test commit

---
+++ samplestack/src/main/java/com/example/demo/SamplestackApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplestackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplestackApplication.class, args);
+	}
+}
