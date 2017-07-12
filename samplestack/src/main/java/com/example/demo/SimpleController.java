1eb52fc8b08764d4c18438fbac3abbf9b3f83eeb
Brian <Brian@LAPTOP-8VND5NEA>
2017-07-12 08:49:43 -0400

    making simple change to see if I can

--- samplestack/src/main/java/com/example/demo/SimpleController.java
+++ samplestack/src/main/java/com/example/demo/SimpleController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SimpleController {
 
+	
+	//comment just to see if I can commit
 	@RequestMapping("/simple")
 	public @ResponseBody String simple() {
 		return "Hello world!";
